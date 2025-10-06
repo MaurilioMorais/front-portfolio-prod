7d865af41d01bc61a1c990daee9a2b7ccc5eaab4
Maurilio Morais <54515627+MaurilioMorais@users.noreply.github.com>
2025-10-05 23:48:34 -0300

    Add files via upload

---
+++ chunk-2VX6AB7O.js
@@ -0,0 +1,1 @@
+var i={production:!1,siteTitle:"Maurilio Morais - Portfolio",siteUrl:"https://maurilio.dev.br",socialLinks:{linkedin:"https://linkedin.com/in/mauriliomorais",github:"https://github.com/mauriliomorais",email:"maurilio976@gmail.com"},analytics:{gtag:""}};export{i as a};
